abd9e5faf5e496d6ef86d2e0694f3f6ffbb9442b
Ondero <Ondero@DESKTOP-163FUDE>
2020-03-09 09:26:23 +0100

    Lehena

---
+++ src/modelo/dao/LibroModelo.java
@@ -0,0 +1,13 @@
+package modelo.dao;
+
+import modelo.Conector;
+
+public class LibroModelo extends Conector{
+	
+	public Libro select(int id) {
+		
+	}
+	public ArrayList<Libro> selectAll{
+		
+	}
+}
