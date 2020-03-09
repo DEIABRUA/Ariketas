a59efe2f392e3d58182204b9dc563fc2b95eaa78
Ondero <Ondero@DESKTOP-163FUDE>
2020-03-09 09:29:34 +0100

    Segundo

--- src/modelo/dao/LibroModelo.java
+++ src/modelo/dao/LibroModelo.java
@@ -1,13 +1,20 @@
 package modelo.dao;
 
+import java.util.ArrayList;
+
 import modelo.Conector;
+import modelo.bean.Libro;
 
 public class LibroModelo extends Conector{
 	
 	public Libro select(int id) {
 		
 	}
-	public ArrayList<Libro> selectAll{
+	public ArrayList<Libro> selectAll(){
+		
+	}
+	
+	boolean exist(int idLibro) {
 		
 	}
 }
